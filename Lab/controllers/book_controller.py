18012512e87d24e2eafb774011551934a087ea83
Annika Neumann <annika1983neumann@gmail.com>
2022-03-16 23:19:04 +0000

    css refactored,background image works,footer,header

--- Lab/controllers/book_controller.py
+++ Lab/controllers/book_controller.py
@@ -26,4 +26,37 @@ def create_book():
     release = request.release['release']
     book = book_repo.select(id)
     book = Book(title, genre, language, author, release)
+    
     book_repo.save(book)
+    return redirect('/book')
+
+@book_blueprint.route("/book/<id>/delete", methods=["POST"])
+def delete_task(id):
+    book_repo.delete(id)
+    return redirect('/book')
+
+@book_blueprint.route("/book/<id>", methods = ['GET'])
+def show_book(id):
+    task = book_repo.select(id)
+    return render_template('book/show.html', book = book)
+
+@book_blueprint.route("/book/<id>/edit", methods =['GET'])
+def edit_book(id):
+    book = book_repo.select(id)
+    authors = authors_repo.select_all()
+    return render_template("/book/edit.html", book = book, all_authors = authors)
+
+@book_blueprint.route("/book/<id>", methods = ['POST'])
+def update_book(id):
+    title = request.form['title']
+    user_id = request.form['user_id']
+    language = request.form['language']
+    authors = request.form['authors']
+    release = request.form['release']
+    genre = request.form['genre']
+    completed = request.form['completed']
+    authors = authors_repo.select(user_id)
+
+    book = Book(title, genre, language, authors, release, id)
+    book_repo.update(book)
+    return redirect('/book')
