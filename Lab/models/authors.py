e079076279f31e19c9b65cca9afcb2c6d41093ad
Annika Neumann <annika1983neumann@gmail.com>
2022-03-16 14:56:14 +0000

    1st commit setup files/class book/authors

---
+++ Lab/models/authors.py
@@ -0,0 +1,6 @@
+class Authors:
+    def __init__(self, name, age, country):
+        self.name = name
+        self.name = age
+        self.country = country
+
---
+++ Lab/models/book.py
@@ -0,0 +1,8 @@
+class Book:
+    def __init__(self, title, genre, language, author, release):
+        self.title = title
+        self.genre = genre
+        self.language = language
+        self.author = author
+        self.release = release
+
