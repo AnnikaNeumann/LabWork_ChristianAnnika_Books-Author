e8bcc619f7de45dc3a293aa8fc9364c2ce08997f
Annika Neumann <annika1983neumann@gmail.com>
2022-03-16 16:37:57 +0000

    Flask run 5000 successful

--- Lab/controllers/book_controller.py
+++ Lab/controllers/book_controller.py
@@ -2,6 +2,28 @@ from flask import Flask, render_template, redirect, request
 
 from flask import Blueprint
 from repositories import authors_repo
-from repositories import book_repository
+from repositories import book_repo
 from models.book import Book
 
+book_blueprint = Blueprint("book", __name__)
+
+@book_blueprint.route("/book")
+def book():
+    book = book_repository.select_all()
+    return render_template("book/index.html", all_book = book)
+
+@book_blueprint.route("/book/new", methods= ['GET'])
+def new_book():
+    book = book_repository.select_all()
+    return render_template("book/new.html", all_book = book)
+
+@book_blueprint.route("/book", methods=['POST'])
+def create_book():
+    title = request.form['title']
+    genre = request.genre['genre']
+    language = request.language['language']
+    author = request.author['author']
+    release = request.release['release']
+    book = book_repo.select(book_id)
+    book = Book(title, genre, language, author, release)
+    book_repo.save(book)
